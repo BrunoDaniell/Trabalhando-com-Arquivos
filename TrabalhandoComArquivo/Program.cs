3b1a1b5450836261da9dd96ab4034b09c8473c35
Bruno Daniel <brunoflemos441@icloud.com>
2023-01-28 18:24:46 -0400

    Atualização de Boas Praticas

--- TrabalhandoComArquivo/Program.cs
+++ TrabalhandoComArquivo/Program.cs
@@ -8,29 +8,27 @@ class TrabComArquivo
     {
 
         string path = @"c:\temp\file1.txt";
-        
-        StreamReader sr = null;
 
         try
         {
-            
-            sr = File.OpenText(path);
-            while (!sr.EndOfStream){
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
-          
+
+
         }
-        catch(IOException e)
+        catch (IOException e)
         {
             Console.WriteLine("An error occurred");
             Console.WriteLine(e.Message);
         }
-        finally
-        {
-            if (sr != null) sr.Close();
-            
-        }
+
 
     }
 
