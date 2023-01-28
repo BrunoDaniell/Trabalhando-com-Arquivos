b3bfbb2be19590058b4601e1e065c5e51833ba27
Bruno Daniel <brunoflemos441@icloud.com>
2023-01-28 18:03:20 -0400

    Exibindo Aquivo

--- TrabalhandoComArquivo/Program.cs
+++ TrabalhandoComArquivo/Program.cs
@@ -7,24 +7,29 @@ class TrabComArquivo
     static void Main(string[] args)
     {
 
-        string sourcePath = @"c:\temp\file1.txt";
-        string targetPath = @"c:\temp\file2.txt";
+        string path = @"c:\temp\file1.txt";
+        
+        StreamReader sr = null;
 
         try
         {
-            FileInfo fileInfo = new FileInfo(sourcePath);
-            fileInfo.CopyTo(targetPath);
-            string[] lines = File.ReadAllLines(sourcePath);
-            foreach(string line in lines)
-            {
+            
+            sr = File.OpenText(path);
+            while (!sr.EndOfStream){
+                string line = sr.ReadLine();
                 Console.WriteLine(line);
             }
-
+          
         }
         catch(IOException e)
         {
             Console.WriteLine("An error occurred");
             Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (sr != null) sr.Close();
+            
         }
 
     }
